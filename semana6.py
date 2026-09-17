34e73462939e5ac2fc3212513f9d7ab6c416a733
lucero <60794255@institutourusayhua.edu.pe>
2026-09-17 12:32:04 -0500

    rama-estudiante

--- semana6.py
+++ semana6.py
@@ -1,3 +1,10 @@
 print ("hola bienbenido a la semana 6")
 print ("hola desde la rama mensaje-mejora")
 
+nombre = input("Ingrese su nombre: ")
+edad = input("Ingrese su edad: ")
+altura = input("Ingrese su altura: ")
+
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Altura:", altura)
